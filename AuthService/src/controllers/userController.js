671ca1a112ac390ab9f4c09df565841609ab549c
Nitigya Chandel <nitigyasingh27@gmail.com>
2025-10-01 19:15:25 +0530

    Made functionality to fetch a user

--- AuthService/src/controllers/userController.js
+++ AuthService/src/controllers/userController.js
@@ -43,9 +43,30 @@ const destroy = async (req,res)=>{
             err:error
         });
     }
-}
+};
+
+const getUser = async (req,res)=>{
+    try {
+        const response = await userService.getUser(req.params.id);
+        return res.status(200).json({
+            data: response,
+            success:true,
+            message:"Successfully fetched a user.",
+            err:{}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:"Something went wrong in controller layer",
+            err:error
+        });
+    }
+};
 
 module.exports={
     create,
-    destroy
+    destroy,
+    getUser
 }
--- AuthService/src/service/userService.js
+++ AuthService/src/service/userService.js
@@ -1,31 +1,38 @@
 const UserRepository = require("../repository/userRepository");
 
-class UserService{
-    constructor(){
-        this.UserRepository = new UserRepository();
+class UserService {
+  constructor() {
+    this.UserRepository = new UserRepository();
+  }
+
+  async create(data) {
+    try {
+      const user = await this.UserRepository.create(data);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong on the service layer.");
+      throw { error };
     }
+  }
+  async destroy(data) {
+    try {
+      const response = await this.UserRepository.destroy(data);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong on the servie layer.");
+      throw { error };
+    }
+  }
 
-    async create(data){
-            try {
-                const user = await this.UserRepository.create(data);
-                return user;
-            } catch (error) {
-                console.log("Something went wrong on the service layer.");
-                throw {error};
-            }
-        }
-    async destroy(data){
-            try {
-                const response = await this.UserRepository.destroy(data);
-                return response;
-            } catch (error) {
-                console.log("Something went wrong on the servie layer.");
-                throw {error};
-            }
-        }
-
-        
-
+  async getUser(userId) {
+    try {
+      const user = await this.UserRepository.getById(userId);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong on the servie layer.");
+      throw { error };
+    }
+  }
 }
 
-module.exports=UserService;
+module.exports = UserService;
