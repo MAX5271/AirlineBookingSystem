a599a97e0b2eda7de554a323f2cb7073414fcf9d
Nitigya Chandel <nitigyasingh27@gmail.com>
2025-10-06 19:47:20 +0530

    added notification model

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -4,7 +4,7 @@ const { PORT } = require("./config/serverConfig");
 
 const apiRoutes = require('./routes/index');
 const { sendBasicEmail } = require('./service/emailService');
-
+const cron = require('node-cron');
 
 const startServer = async () => {
   const app = express();
@@ -15,7 +15,16 @@ const startServer = async () => {
 
   app.listen(PORT,()=>{
     console.log(`Server has started on the port ${PORT}`);
+    // sendBasicEmail(
+    //   'support@admin.com',
+    //   'airlinebookingproject420@gmail.com',
+    //   'This is a test email',
+    //   'I hope you like the support'
+    // );
   });
+  // cron.schedule('* * * * *', () => {
+  //   console.log('running a task every minute');
+  // });
 };
 
 startServer();
